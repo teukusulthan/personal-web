b7643fdeab6584f88c8330b776808777b70147e1
teukusulthan <teukusultanul@gmail.com>
2025-11-05 17:54:09 +0700

    fix: hero section responsivity

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,14 +6,11 @@ import Link from "next/link";
 
 export default function Hero() {
   return (
-    <section
-      id="hero"
-      className="h-screen px-5 md:px-10 pt-20 sm:pt-0 md:pt-20 "
-    >
-      <div className="mx-auto flex h-full max-w-7xl items-start md:items-center pt-12 sm:pt-16 md:pt-0 px-6 sm:px-8 md:px-12 lg:px-20">
+    <section id="hero" className="h-screen md:px-10 pt-20 sm:pt-0 md:pt-20 ">
+      <div className="mx-auto flex h-full max-w-7xl items-start md:items-center pt-8 sm:pt-16 md:pt-0 px-6 sm:px-8 md:px-12 lg:px-20">
         <div className="grid w-full grid-cols-1 items-center gap-8 md:grid-cols-12">
           <div className="order-1 md:order-2 md:col-span-5">
-            <div className="mx-auto w-48 sm:w-56 md:w-64 lg:w-80">
+            <div className="mx-auto w-48 sm:w-56 lg:w-80">
               <div className="relative aspect-4/5 overflow-hidden rounded-md ring-1 ring-border/60">
                 <Image
                   src={sulthan}
@@ -29,10 +26,13 @@ export default function Hero() {
 
           <div className="order-2 md:order-1 md:col-span-7 text-center md:text-left">
             <h1 className="text-2xl font-bold tracking-tight sm:text-4xl lg:text-5xl">
-              Hi, I’m Teuku Sulthan.
+              <span className="block md:inline text-muted-foreground">
+                HI, I'M{" "}
+              </span>
+              TEUKU SULTHAN.
             </h1>
             <h2 className="mt-2 text-md text-foreground/80 sm:text-2xl">
-              Full Stack Software Engineer
+              FULL STACK SOFTWARE ENGINEER
             </h2>
             <p className="mt-6 max-w-2xl text-sm leading-7 text-foreground/80 sm:text-md mx-auto md:mx-0">
               I design, build, and operate digital products end-to-end
